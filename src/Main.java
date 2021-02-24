92a0aa673012f60d4174fcaef54a34dd8b1c242b
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-02-24 20:44:22 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        final int a = 3;
+        final int b = 5;
+
+        final int c = a * b;
+
+        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+    }
+}
